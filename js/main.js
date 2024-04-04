921bf57448ee6af6f9a54b0f5e7f5aa156306070
kgarcia18 <126489822+kgarcia18@users.noreply.github.com>
2024-04-04 09:28:19 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+import { lista_de_componentes } from "./Data.js";
+
+function cargar_div(){
+
+    let div = document.querySelector("#root");
+
+    lista_de_componentes.forEach((cada_tarea) => {
+
+
+        let div_tarea = document.createElement("div");
+        div_tarea.classList.add("div_tareas");
+    
+        div_tarea.innerHTML=`
+            <div class="caja">
+                <h1 class="titulo">${cada_tarea.nombre}</h1>
+                <p class="texto texto">${cada_tarea.descripcion}</p>
+                <button class="boton">Boton</button>
+            </div>
+        `
+    
+        div.appendChild(div_tarea);
+    
+    });
+}
+cargar_div();
+
